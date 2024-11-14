c6a1156c429c08fe3bdf0b1475b2c62d6e8e6240
sylindrical <turtlerbean@protonmail.com>
2024-11-14 14:39:58 +0000

    Additional rudimentary front-page setup

---
+++ Fatherboard/resources/views/about.blade.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset("css/main.css")}}">
+    <title>Document</title>
+</head>
+<body>
+    <title>Fatherboard</title>
+
+    <nav id="navigation">
+        <ul>
+            <li><img id="logo" src="{{ asset("images/logo.png")}}"/></li>
+            <li>Home</li>
+            <li>About</li>
+            <li>Settings</li>
+        </ul>
+        </nav>
+    
+        <p>
+
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus posuere quam vitae cursus consectetur. Suspendisse pharetra elit risus, ut rutrum nisl suscipit vel. Ut sodales nulla quis velit sodales, a rutrum augue convallis. Nullam euismod felis sem, sed dapibus turpis congue id. Vivamus cursus velit quis pharetra volutpat. Cras varius, massa in tempor sodales, urna lorem lacinia lectus, vel congue arcu urna ac odio. Fusce felis sapien, semper a mi vitae, congue pharetra mauris. Suspendisse in magna quis nunc egestas imperdiet. Donec tempus at dolor sit amet rhoncus. Fusce orci nisi, pulvinar at leo a, molestie ultrices orci.
+            
+            Pellentesque convallis mi et urna sodales, nec varius dui viverra. Suspendisse viverra interdum tristique. Proin augue eros, dictum ac lacus quis, malesuada consequat felis. Nulla egestas laoreet dapibus. Suspendisse massa mauris, tempus a porttitor a, facilisis eget ante. Pellentesque molestie tellus sit amet fringilla efficitur. Donec sagittis lectus ut mollis condimentum. Vivamus blandit, orci vitae gravida sodales, libero libero rutrum velit, nec tincidunt augue mauris vitae dui. Aliquam accumsan nec arcu ut dictum. Integer in rhoncus ante, quis tincidunt neque. Ut ac magna magna. Vivamus aliquet turpis id placerat faucibus. Vestibulum diam mauris, fringilla id lectus vel, egestas auctor quam. Praesent aliquam orci non orci auctor, vitae porta eros egestas.
+            
+            Nunc commodo at mauris at molestie. Donec blandit a nulla eget ullamcorper. Nullam rhoncus metus eget erat aliquet, a viverra orci pulvinar. Ut et sodales nisl, non varius erat. Donec malesuada velit volutpat massa luctus, a venenatis eros tincidunt. Donec faucibus vulputate lacus, quis faucibus turpis. Curabitur dapibus elementum mattis. Sed et ante in quam tristique euismod sed vel arcu. Nullam a enim finibus purus rhoncus accumsan sed sit amet arcu. Ut eleifend tincidunt sem, ut fringilla eros sollicitudin a. Quisque blandit scelerisque maximus. Proin finibus leo non dui mollis ultrices. Maecenas placerat cursus tempus.
+            
+            Fusce tempor, tellus et ullamcorper maximus, nunc tellus facilisis enim, ac varius justo risus non tortor. Nulla in nisi nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi dui mauris, vestibulum vitae ullamcorper id, lacinia suscipit arcu. Ut mauris orci, pellentesque vel quam sed, ornare vehicula ipsum. Donec eleifend vehicula nisi, eget eleifend dolor viverra a. Nam euismod finibus fermentum. Nam consectetur libero enim, eu ornare nisi tempus at. Nunc consequat ipsum ac felis pellentesque ornare. Phasellus erat arcu, tincidunt vitae viverra quis, semper ut mi. Fusce et felis id ante consequat convallis. Aliquam non porta nisi. Nam hendrerit, ex sit amet accumsan efficitur, velit metus gravida tellus, faucibus suscipit nibh est sit amet eros. Sed porta hendrerit risus at sollicitudin.
+            
+            Suspendisse ut vestibulum sem. Phasellus lacus justo, gravida eget urna id, commodo finibus nunc. Nullam in enim vel elit accumsan interdum ut a tortor. Vivamus aliquam quis tellus sit amet egestas. In turpis enim, vestibulum ac egestas at, maximus sed neque. Duis a posuere metus. Donec ornare pharetra dictum. Phasellus sed lorem tempus, lobortis lectus vitae, cursus dolor. Mauris vitae magna in risus ultricies maximus sit amet nec nisl. Nam id ante eget ante tempor bibendum vitae eget arcu. </p>
+</body>
+</html>
---
+++ Fatherboard/resources/views/settings.blade.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset("css/main.css")}}">
+    <title>Document</title>
+</head>
+<body>
+    <title>Fatherboard</title>
+
+    <nav id="navigation">
+        <ul>
+            <li><img id="logo" src="{{ asset("images/logo.png")}}"/></li>
+            <li></a href="./">Home</a></li>
+            <li><a href="./about">About</a></li>
+            <li></a href="./settings">Settings</a></li>
+        </ul>
+        </nav>
+</body>
+</html>
