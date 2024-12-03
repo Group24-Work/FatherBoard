fdd4faed0ce0f9569ffc600dd05f197577387985
sylindrical <turtlerbean@protonmail.com>
2024-12-03 18:44:25 +0000

    modifying settings page with lewis' filtering work

--- Fatherboard/public/js/products.js
+++ Fatherboard/public/js/products.js
@@ -1,140 +1,87 @@
+function change() { // https://stackoverflow.com/a/48316156
+    var categoryCheckbox = document.querySelectorAll(".categories input[type='checkbox']");
+    var priceCheckbox = document.querySelectorAll(".prices input[type='checkbox']");
+    var filters = {
+      categories: getClassOfCheckedCheckboxes(categoryCheckbox),
+      prices: getClassOfCheckedCheckboxes(priceCheckbox)
+    };
+  
+    filterResults(filters);
+  }
+  
+  function getClassOfCheckedCheckboxes(checkboxes) {
+    var classes = [];
+  
+    if (checkboxes && checkboxes.length > 0) {
+      for (var i = 0; i < checkboxes.length; i++) {
+        var cb = checkboxes[i];
+  
+        if (cb.checked) {
+          classes.push(cb.getAttribute("rel"));
+        }
+      }
 
-
-let productIdentifier = "product_identity";
-
-
-let button_filter = null;
-
-let menu_product_type = null;
-
-
-
-class ProductElement extends HTMLElement
-{
-    constructor()
-    {
-        super();
-        this.attachShadow({mode : "open"});
-        let template_product  = document.getElementById("template_product");
-
-        this.shadowRoot.append(template_product.content.cloneNode(true));
-
+      console.log(classes)
     }
-}
-
-customElements.define(
-    "product-element",
-    ProductElement
-)
-
-
-window.addEventListener("DOMContentLoaded", function()
-{
-    button_filter = document.getElementById("filter-button");
-    menu_product_type = document.getElementById("product_type");
-
-
-
-
-    button_filter.addEventListener("click",filterClick)
-    
-addLinks();
-
-}
-)
-
-function addLinks()
-{
-    let items = document.querySelectorAll(".ProductItem");
-
-    items.forEach((x)=>{
-        let y = parseInt((x.children[0].textContent));
-
-        x.addEventListener("click",()=>productClick(y))
-    });
-}
-
-
-function filterClick()
-{
-    let type = menu_product_type.value;
-    let csrf = document.getElementsByName("csrf-token");
-    let csrf_token = csrf[0].getAttribute("content");
-    console.log(csrf_token);
-    fetch("/products", {method : "POST", headers :{
-                    'Content-Type': 'application/json',
-                    "X-CSRF-TOKEN" : csrf_token} ,body : JSON.stringify({category : type })}).then((x)=>x.json()).then((dec)=>
-                    {
-                        console.log(dec);
-                        showProduct(dec);
-                    })
-}
-
-
-function showProduct(info)
-{
-    let product_container = document.getElementById("ProductContainer");
-
-    product_container.innerHTML = "";
-
-    if (info.length != 0)
-    {
-
-    for (let elem of info)
-    {
-    let product_element = document.createElement("product-element");
-    product_element.setAttribute("class","ProductItem")
-
-
-    let id_text = document.createElement("p")
-    id_text.hidden = true;
-    id_text.textContent = elem["id"]
-
-    let title_text = document.createElement("span")
-    title_text.setAttribute("slot","Title");
-    title_text.textContent = elem["Title"];
-
-    let description_text = document.createElement("span")
-    description_text.setAttribute("slot","Description");
-    description_text.textContent = elem["Description"];
-
-    let manufacturer_text = document.createElement("span")
-    manufacturer_text.setAttribute("slot","Manufacturer");
-    manufacturer_text.textContent = elem["Manufacturer"];
-
-    product_element.appendChild(id_text);
-
-    product_element.appendChild(title_text);
-    product_element.appendChild(description_text);
-    product_element.appendChild(manufacturer_text);
-
-    
-    product_container.append(product_element);
-
+  
+    return classes;
+  }
+  
+  function filterResults(filters) {
+    var rElems = document.querySelectorAll(".result div");
+    var hiddenElems = [];
+  
+    if (!rElems || rElems.length <= 0) {
+      return;
     }
-}
-    else
-    {
-
-        let info_title = document.createElement("h3");
-        info_title.textContent = "Product Information";
-
-
-        let info_elem = document.createElement("p");
-        info_elem.textContent = "You do not have an address currently.";
-
-        product_container.append(info_title);
-        product_container.append(info_elem);
-
+  
+    for (var i = 0; i < rElems.length; i++) {
+      var el = rElems[i];
+  
+      if (filters.categories.length > 0) {
+        var isHidden = true;
+  
+        for (var j = 0; j < filters.categories.length; j++) {
+          var filter = filters.categories[j];
+  
+          if (el.classList.contains(filter)) {
+            isHidden = false;
+            break;
+          }
+        }
+  
+        if (isHidden) {
+          hiddenElems.push(el);
+        }
+      }
+  
+      if (filters.prices.length > 0) {
+        var isHidden = true;
+  
+        for (var j = 0; j < filters.prices.length; j++) {
+          var filter = filters.prices[j];
+  
+          if (el.classList.contains(filter)) {
+            isHidden = false;
+            break;
+          }
+        }
+  
+        if (isHidden) {
+          hiddenElems.push(el);
+        }
+      }
     }
-    addLinks()
-
-
-
-}
-function productClick(num)
-{
-console.log("clicked" + num);
-// Simulate a mouse click:
-window.location.href = "./product/" + num;
-}
+  
+    for (var i = 0; i < rElems.length; i++) {
+      rElems[i].style.display = "block";
+    }
+  
+    if (hiddenElems.length <= 0) {
+      return;
+    }
+  
+    for (var i = 0; i < hiddenElems.length; i++) {
+      hiddenElems[i].style.display = "none";
+    }
+  }
--- Fatherboard/resources/views/products.blade.php
+++ Fatherboard/resources/views/products.blade.php
@@ -1,49 +1,112 @@
+
 <x-lowlayout>
-
     <x-slot:head>
-        <link rel="stylesheet" href={{ asset('css/products.css') }}>
-        <script src={{ asset('js/products.js') }}></script>
-        <title>Products</title>
+        <link rel="stylesheet" href="{{asset('css/products.css')}}">
+        <script src="{{asset('/js/products.js')}}"></script>
     </x-slot:head>
-    <h2>Products</h2>
 
-<br>
-<meta name="csrf-token" content="{{ csrf_token() }}">
-<select name="type" id="product_type">
-    <option value="gpu">GPU</option>
-    <option value="cpu">CPU</option>
-    <option value="psu">PSU</option>
-    <option value="memory">Memory</option>
-    <option value="prebuilt-pcs">Pre-built pcs</option>
-</select>
+<div class="wrapper">
+    <div class="filter-sidebar filter"> <!--https://dev.to/clairecodes/how-to-make-a-sticky-sidebar-with-two-lines-of-css-2ki7-->
+        <h3>Filter By:</h3>
+        <div class="categories"> <!--https://stackoverflow.com/a/48316156-->
+            <h4 style="padding-left: 1.563rem;">Category:</h4>
+            <br>
+            <div class="checkbox">
+                <label><input type="checkbox" rel="Memory" onchange="change();"/>Memory</label>
+                </div>   
+            <br>
+            <div class="checkbox">
+                <label><input type="checkbox" rel="CPUs" onchange="change();" href="CPUs"/>CPUs</label>
+            </div>   
+            <br>
+            <div class="checkbox">
+                <label><input type="checkbox" rel="Prebuilts" onchange="change();"/>Prebuilt Computers</label>
+            </div>   
+            <br>
+            <div class="checkbox">
+                <label><input type="checkbox" rel="GPUs" onchange="change();"/>GPUs</label>
+            </div>   
+            <br>
+            <div class="checkbox">
+                <label><input type="checkbox" rel="PSUs" onchange="change();"/>PSUs</label>
+            </div>   
+            <br>
+            <div class="checkbox">
+                <label><input type="checkbox" rel="Sale" onchange="change();"/>On Sale</label>
 
-<button id="filter-button">Filter</button>
+            </div>
+        </div>
 
-<template  id="template_product">
-    <h2><slot name="Title">Unknown Title</slot></h2>
-    <p><slot name="Description">Unknown Description</slot></p>
-    <p><slot name="Manufacturer">Unknown Manufacturer</slot></p>
-</template>
+        <br>
+        <hr>
+        <br>
+
+        <h4 style="padding-left: 1.563rem;">Price:</h4>
+        <br>
+
+        <div class="prices">
+            <div class="checkbox">
+                <label><input type="checkbox" rel="50-100" onchange="change();"/> £50-100 </label>
+            </div>
+            <br>
+
+            <div class="checkbox">
+                <label><input type="checkbox" rel="100-200" onchange="change();"/> £100-200 </label>
+            </div>
+            <br>
+            <div class="checkbox">
+                <label><input type="checkbox" rel="200-300" onchange="change();"/>£200-300 </label>
+            </div>
+            <br>
+
+            <div class="checkbox">
+                <label><input type="checkbox" rel="300-400" onchange="change();"/>£300-400</label>
+            </div>
+            <br>
+
+            <div class="checkbox">
+                <label><input type="checkbox" rel="400-500" onchange="change();"/> £400-500</label>
+            </div>
+            <br>
+
+            <div class="checkbox">
+                <label><input type="checkbox" rel="500+" onchange="change();"/> £500+ </label>
+            </div>
+
+        </div>
 
 
-<div id="ProductContainer">
-    <?php
-    foreach($data as $x=>$item)
-    {
+        </p>
+        </div>
+
+        <template  id="template_product">
+            <h2><slot name="Title">Unknown Title</slot></h2>
+            <p><slot name="Description">Unknown Description</slot></p>
+            <p><slot name="Manufacturer">Unknown Manufacturer</slot></p>
+        </template>
         
-            ?>
-
-
-            <product-element class="ProductItem">
-                <p hidden class="product_identity"> {{$item["id"]}} </p>
-                <span slot="Title">{{ $item['Title'] }}</span>
-                <span slot="Description">{{ $item['Description'] }}</span>
-                <span slot="Manufacturer"> {{ $item['Manufacturer']  }}</span>
-
-            </product-element>
+    <div class="main-content">
+        <h2>Main Content</h2>
+        <div id="ProductContainer">
+            <div class="CPUs 100-200">
+                <h2>CPU 1</h2>
+            </div>
             <?php
-
-    }
-    ?>
+                foreach ($data as $index=>$item)
+                {
+                    ?>
+                    <product-element class="ProductItem">
+                        <p hidden class="product_identity"> {{$item["id"]}} </p>
+                        <span slot="Title">{{ $item['Title'] }}</span>
+                        <span slot="Description">{{ $item['Description'] }}</span>
+                        <span slot="Manufacturer"> {{ $item['Manufacturer']  }}</span>
+        
+                    </product-element>
+                    <?php
+                }
+            ?>
+        </div>
     </div>
+</div>
+
 </x-lowlayout>
