5012cb21857dabf4535edf6fb071ddd52334ff6b
LewisForster <lforst512@gmail.com>
2025-02-21 18:31:26 +0000

    Added icons, fixed carousel scrolling - bug with icons disappearing on pressing back (firefox specific)

--- Fatherboard/resources/views/home.blade.php
+++ Fatherboard/resources/views/home.blade.php
@@ -4,7 +4,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>FatherBoard - Homepage</title>
         <link rel="stylesheet" type="text/css" href="{{asset('css/aboutus.css')}}"> <!-- Link for the header styles -->
-        <link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}"> <!-- Link for homepage-specific styles -->
+        <link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}">
+        <script src="{{asset('js/carousel.js')}}" defer></script> <!-- Link for homepage-specific styles -->
     </x-slot:head>
 
 
