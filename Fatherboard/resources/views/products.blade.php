430b5148058a0d8bf7410017ce56c030eba18e1d
romistry19 <romistry10@gmail.com>
2025-02-26 15:20:36 +0000

    Revert "Changing to fit theme trying to add images to product page"
    This reverts commit 1bb340f6f7e93cdb4583bd10ff3fdf2734da10da.

--- Fatherboard/resources/views/products.blade.php
+++ Fatherboard/resources/views/products.blade.php
@@ -1,3 +1,4 @@
+
 <x-lowlayout>
     <x-slot:head>
         <link rel="stylesheet" href={{ asset('css/products.css') }}>
@@ -6,124 +7,116 @@
         <title>Products</title>
     </x-slot:head>
 
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
 
 
-    <template id="template_product">
-        <style>
-            .price {
-                color: black;
-            }
+<template  id="template_product">
+    <style>
+        .price
+        {
+            color:black;
+        }
         </style>
-
-        <h2>
-            <slot name="Title">Unknown Title</slot>
-        </h2>
-        <p>
-            <slot name="Description">Unknown Description</slot>
-        </p>
-        <p>
-            <slot class="price" name="Manufacturer">Unknown Manufacturer</slot>
-        </p>
-    </template>
-    <!-- header.html -->
-    <x-header></x-header>
+    <h2><slot name="Title">Unknown Title</slot></h2>
+    <p><slot name="Description">Unknown Description</slot></p>
+    <p><slot class="price" name="Manufacturer">Unknown Manufacturer</slot></p>
+</template>
+<!-- header.html -->
+<x-header></x-header>
 
 
 
 
 
 
-    <div class="filter-sidebar filter">
-        <!--https://dev.to/clairecodes/how-to-make-a-sticky-sidebar-with-two-lines-of-css-2ki7-->
-        <h3>Filter By:</h3>
-        <div class="categories"> <!--https://stackoverflow.com/a/48316156-->
-            <h4 style="padding-left: 1.563rem;">Category:</h4>
-            <br>
-            <div class="checkbox">
-                <label><input type="checkbox" rel="Memory" />Memory</label>
+<div class="filter-sidebar filter"> <!--https://dev.to/clairecodes/how-to-make-a-sticky-sidebar-with-two-lines-of-css-2ki7-->
+    <h3>Filter By:</h3>
+    <div class="categories"> <!--https://stackoverflow.com/a/48316156-->
+        <h4 style="padding-left: 1.563rem;">Category:</h4>
+        <br>
+        <div class="checkbox">
+            <label><input type="checkbox" rel="Memory"/>Memory</label>
             </div>
-            <br>
-            <div class="checkbox">
-                <label><input type="checkbox" rel="CPU" href="CPUs" />CPUs</label>
-            </div>
-            <br>
-            <div class="checkbox">
-                <label><input type="checkbox" rel="Prebuilt" />Prebuilt Computers</label>
-            </div>
-            <br>
-            <div class="checkbox">
-                <label><input type="checkbox" rel="GPU" />GPUs</label>
-            </div>
-            <br>
-            <div class="checkbox">
-                <label><input type="checkbox" rel="PSU" />PSUs</label>
-            </div>
-            <br>
-
+        <br>
+        <div class="checkbox">
+            <label><input type="checkbox" rel="CPU" href="CPUs"/>CPUs</label>
         </div>
-
         <br>
-        <hr>
-        <br>
-
-        <h4 style="padding-left: 1.563rem;">Price:</h4>
-        <br>
-
-        <div class="prices">
-            <div class="checkbox">
-                <label><input type="checkbox" rel="50-100" /> £50-100 </label>
-            </div>
-            <br>
-
-            <div class="checkbox">
-                <label><input type="checkbox" rel="100-200" /> £100-200 </label>
-            </div>
-            <br>
-            <div class="checkbox">
-                <label><input type="checkbox" rel="200-300" />£200-300 </label>
-            </div>
-            <br>
-
-            <div class="checkbox">
-                <label><input type="checkbox" rel="300-400" />£300-400</label>
-            </div>
-            <br>
-
-            <div class="checkbox">
-                <label><input type="checkbox" rel="400-500" /> £400-500</label>
-            </div>
-            <br>
-
-            <div class="checkbox">
-                <label><input type="checkbox" rel="500+" /> £500+ </label>
-            </div>
-
+        <div class="checkbox">
+            <label><input type="checkbox" rel="Prebuilt" />Prebuilt Computers</label>
         </div>
-        <button id="filter-button" style="background-color: purple; color: white;">Filter</button>
+        <br>
+        <div class="checkbox">
+            <label><input type="checkbox" rel="GPU" />GPUs</label>
+        </div>
+        <br>
+        <div class="checkbox">
+            <label><input type="checkbox" rel="PSU" />PSUs</label>
+        </div>
+        <br>
 
     </div>
-    <div id="ProductContainer">
-        <?php
-if (count($data) > 0) {
-    foreach ($data as $item) {
+
+    <br>
+    <hr>
+    <br>
+
+    <h4 style="padding-left: 1.563rem;">Price:</h4>
+    <br>
+
+    <div class="prices">
+        <div class="checkbox">
+            <label><input type="checkbox" rel="50-100" /> £50-100 </label>
+        </div>
+        <br>
+
+        <div class="checkbox">
+            <label><input type="checkbox" rel="100-200"/> £100-200 </label>
+        </div>
+        <br>
+        <div class="checkbox">
+            <label><input type="checkbox" rel="200-300" />£200-300 </label>
+        </div>
+        <br>
+
+        <div class="checkbox">
+            <label><input type="checkbox" rel="300-400" />£300-400</label>
+        </div>
+        <br>
+
+        <div class="checkbox">
+            <label><input type="checkbox" rel="400-500" /> £400-500</label>
+        </div>
+        <br>
+
+        <div class="checkbox">
+            <label><input type="checkbox" rel="500+" /> £500+ </label>
+        </div>
+
+    </div>
+    <button id="filter-button">Filter</button>
+
+</div>
+<div id="ProductContainer">
+    <?php
+    if (count($data) > 0) {
+    foreach($data as $item)
+    {
             ?>
 
 
-        <product-element class="ProductItem">
+            <product-element class="ProductItem">
+                <p hidden class="product_identity"> {{$item["id"]}} </p>
+                <span slot="Title">{{ $item['Title'] }}</span>
+                <span slot="Description">{{ $item['Description'] }}</span>
+                <span slot="Manufacturer"> {{ $item->price()->first()["price"] }}</span>
 
-            <p hidden class="product_identity"> {{$item["id"]}} </p>
-            <span slot="Title">{{ $item['Title'] }}</span>
-            <span slot="Description">{{ $item['Description'] }}</span>
-            <span slot="Manufacturer"> {{ $item->price()->first()["price"] }}</span>
+            </product-element>
+            <?php
 
-        </product-element>
-        <?php
-
-    }
-}
-;
+    }};
     ?>
     </div>
 </x-lowlayout>
+
