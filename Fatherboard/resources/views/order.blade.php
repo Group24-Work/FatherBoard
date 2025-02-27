048a6c9fada050e026ad3402988006fba2afdac6
sylindrical <turtlerbean@protonmail.com>
2025-02-27 22:00:14 +0000

    Added return button that follows purple color scheme in order items page

--- Fatherboard/resources/views/order.blade.php
+++ Fatherboard/resources/views/order.blade.php
@@ -10,12 +10,20 @@
 
 
 
+    
+    
+
     <div id="return_container">
-        <b>Return Back</b>
+        <a href="/settings">
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="-2 -2 24 24" stroke=currentColor>
+            <g transform="scale(-1,1) translate(-20,0)">
+                <path d="M10 20A10 10 0 1 0 0 10a10 10 0 0 0 10 10zM8.711 4.3l5.7 5.766L8.7 15.711l-1.4-1.422 4.289-4.242-4.3-4.347z"/>
+            </g>
+        </svg>
+    </a>
     </div>
-
-
-
+    
+    
     <div id="order_block">
         <p>{{}}</p>
     <div id="order_container">
