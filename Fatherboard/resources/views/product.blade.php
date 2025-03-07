862ef8a774cda64ee443a3eb2a548c39b6bfe9ec
Rohan Mistry <romistry10@gmail.com>
2025-03-07 15:35:47 +0000

    Update product.blade.php

--- Fatherboard/resources/views/product.blade.php
+++ Fatherboard/resources/views/product.blade.php
@@ -28,8 +28,8 @@
             <h2 id="title">{{ $product->Title}} </h2>
             <p>{{ $product["Description"]}} </p>
             <p>{{ $product["Manufacturer"] }}</p>
-            <p>Price: £{{ $product->price }}</p>
-
+            <p>£{{ $product["Price"] }}</p> 
+            
 
             <form action="{{ route('basketAdd') }}" method="POST">
                 @csrf
