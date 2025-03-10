90ce89b50491ef7ef917a0d35e8b173ca479f8f6
sylindrical <turtlerbean@protonmail.com>
2025-03-10 19:33:10 +0000

    Created relationship for product and its stock

--- Fatherboard/app/Models/ProductStock.php
+++ Fatherboard/app/Models/ProductStock.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductStock extends Model
 {
-    protected $fillable = ["Stock"];
+    protected $fillable = ["Stock", "product_id"];
+
+    public function products()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
