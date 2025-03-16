d123038a61a35b244aea6fa4cece3a9d66129a37
LewisForster <lforst512@gmail.com>
2025-03-16 14:42:58 +0000

    Updated my python script to generate a random password. This random password will be returned so it can be used to reset the user password.

--- Fatherboard/resources/python/emailScript.py
+++ Fatherboard/resources/python/emailScript.py
@@ -1,21 +1,27 @@
 import smtplib
 from email.message import EmailMessage
+import random
+import string
 
+def passGen(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
-message =  EmailMessage()
+def sendEmail():
+    message =  EmailMessage()
+    content = passGen()
+    message.set_content(f"Your new password is: {content}")
+    message['Subject'] = "Reset Password Request"
+    message['From'] = "fatherboard321@gmail.com"
+    message['To'] = "mohammedridwan@protonmail.com"
+    smtp_server= smtplib.SMTP('smtp.gmail.com', 587)
+    smtp_server.ehlo()
+    smtp_server.starttls()
 
-message.set_content("Your new password is:")
-message['Subject'] = "Reset Password Request"
-message['From'] = "fatherboard321@gmail.com"
-message['To'] = "mohammedridwan@protonmail.com"
+    smtp_server.login("fatherboard321@gmail.com","lfuj hhbm vura eewj")
 
+    smtp_server.send_message(message)
+    smtp_server.quit()
+    print("success!")
+    return content
 
-
-smtp_server= smtplib.SMTP('smtp.gmail.com', 587)
-smtp_server.ehlo()
-smtp_server.starttls()
-
-smtp_server.login("fatherboard321@gmail.com","lfuj hhbm vura eewj")
-
-smtp_server.send_message(message)
-smtp_server.quit()
+sendEmail()
