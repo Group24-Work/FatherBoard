cf0a5e58a4d2188c7b29b5038d01f4fda2fa7c52
sylindrical <turtlerbean@protonmail.com>
2025-03-20 15:56:40 +0000

    Made several links now point to current resources + created exit button

--- Fatherboard/resources/views/admin_hub.blade.php
+++ Fatherboard/resources/views/admin_hub.blade.php
@@ -36,7 +36,7 @@
         <div id="charts">
         <h2>Welcome Admin</h2>
         
-            <div id="revenue_section">
+            <div class="chart_section" id="revenue_section">
                 <button class="week">Week</button>
                 <button class="month">Month</button>
                 <div class="chartContainer" id="revenueContainer">
@@ -45,7 +45,7 @@
             </div>
             
 
-            <div id="revenueType_section">
+            <div class="chart_section" id="revenueType_section">
                 <button class="week">Week</button>
                 <button class="month">Month</button>
                 <div class="chartContainer" id="revenueTypeContainer">
@@ -53,7 +53,7 @@
                 </div>
             </div>
 
-            <div id="registrationType_section">
+            <div class="chart_section" id="registrationType_section">
                 <div class="chartContainer" id="registrationContainer">
                     <canvas class="chart" id="registration_chart"></canvas>
                 </div>
@@ -61,7 +61,7 @@
         </div>
 
 
-        <div id="action_contaner">
+        <div id="action_container">
             <div>
                 <p>Overview</p>
             </div>
@@ -74,15 +74,24 @@
     
                 <p>Account Management</p>
             </div>
-    
             <div>
-    
-                <p>Product Management</p>
+                <a href="{{route("tagpage")}}">
+                    <p>Tag</p>
+                </a>
+            </div>
+            <div>
+                <a href="{{route("tagindex")}}">
+                    <p>Product Management</p>
+
+                </a>
             </div>
     
             <div>
+                <a href="{{route("messages")}}">
+                    <p>Messages</p>
+
+                </a>
     
-                <p>Messages</p>
             </div>
     
     
@@ -116,9 +125,11 @@
 
 
     <div id="exit">
+        <a href="{{asset(route("settings"))}}">
         <p>Exit</p>
 
         <svg class="icon" fill="#000000" height="3rem" width="3rem" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 56 56" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M54.424,28.382c0.101-0.244,0.101-0.519,0-0.764c-0.051-0.123-0.125-0.234-0.217-0.327L42.208,15.293 c-0.391-0.391-1.023-0.391-1.414,0s-0.391,1.023,0,1.414L51.087,27H20.501c-0.552,0-1,0.447-1,1s0.448,1,1,1h30.586L40.794,39.293 c-0.391,0.391-0.391,1.023,0,1.414C40.989,40.902,41.245,41,41.501,41s0.512-0.098,0.707-0.293l11.999-11.999 C54.299,28.616,54.373,28.505,54.424,28.382z"></path> <path d="M36.501,33c-0.552,0-1,0.447-1,1v20h-32V2h32v20c0,0.553,0.448,1,1,1s1-0.447,1-1V1c0-0.553-0.448-1-1-1h-34 c-0.552,0-1,0.447-1,1v54c0,0.553,0.448,1,1,1h34c0.552,0,1-0.447,1-1V34C37.501,33.447,37.053,33,36.501,33z"></path> </g> </g></svg>
+    </a>
     </div>
 
 </x-lowlayout>
