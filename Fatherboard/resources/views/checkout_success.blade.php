6d30e820b68e61d50129dee191cddd827c5250e5
LewisForster <lforst512@gmail.com>
2025-03-21 15:43:30 +0000

    Generates an order number, ensures theres no collisions - displays on checkout, fixed checkout routing

--- Fatherboard/resources/views/checkout_success.blade.php
+++ Fatherboard/resources/views/checkout_success.blade.php
@@ -3,31 +3,24 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>FatherBoard - Checkout</title>
-        <link rel="stylesheet" type="text/css" href="{{asset('css/aboutus.css')}}"> <!-- Link for the header styles -->
-        <link rel="stylesheet" type="text/css" href="{{asset('css/checkout_success.css')}}"> <!-- Link for homepage-specific styles -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/aboutus.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/checkout_success.css') }}">
     </x-slot:head>
     <x-header></x-header>
     <x-slot:title>
-        success
+        Success
     </x-slot:title>
-    <div class=wrapper>
-        <div id=wrapperHeader>
+    <div class="wrapper">
+        <div id="wrapperHeader">
             <h1>Order Confirmed!</h1>
         </div>
-        <div id=wrapperSubtitle>
+        <div id="wrapperSubtitle">
             <p>Thank you for your order!</p>
         </div>
-        <?php
-        if (isset($order) && $order > 0)
-            ?>
-            <div id=orderNumber>
-                <p>Your order number is: {{$orderNumber}}</p>
+        @if (isset($orderNumber) && $orderNumber)
+            <div id="orderNumber">
+                <p>Your order number is: #{{ $orderNumber }}</p>
             </div>
-
-        }
-        }
-    }
-    <x-slot:sheet>
-    </x-slot:sheet>
+        @endif
+    </div>
 </x-lowlayout>
-}
