7a990ddbd3d8ce169f1037b53eb240656aae59d3
romistry19 <romistry10@gmail.com>
2025-03-21 20:28:10 +0000

    attempt to fix styling for dropdown will fix tomorrow

--- Fatherboard/resources/views/components/header.blade.php
+++ Fatherboard/resources/views/components/header.blade.php
@@ -11,13 +11,15 @@
 
         <nav class="main-nav">
             <ul class="main-nav-list">
-                <li><a href="/settings"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                            viewBox="0 0 24 24" fill="currentColor"
-                            class="icon icon-tabler icons-tabler-filled icon-tabler-user">
+                <li>
+                    <a href="/settings">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-user">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                             <path d="M12 2a5 5 0 1 1 -5 5l.005 -.217a5 5 0 0 1 4.995 -4.783z" />
                             <path d="M14 14a5 5 0 0 1 5 5v1a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-1a5 5 0 0 1 5 -5h4z" />
-                        </svg></a>
+                        </svg>
+                    </a>
                     <ul class="dropdown">
                         <li><a href="/settings">Settings</a></li>
                         <li><a href="/settings#!history">History</a></li>
@@ -38,10 +40,11 @@
             <ul class="lower-nav-list">
                 <li>
                     <a href="/products?category=memory">Memory</a>
-                    <a href="/products?category=cpu">CPUs</a>
-                    <a href="/products?category=prebuilt">Prebuilt Computers</a>
-                    <a href="/products?category=gpu">GPUs</a>
-                    <a href="/products?category=psu">PSUs</a>
+                </li>
+                <li> <a href="/products?category=cpu">CPUs</a></li>
+                <li> <a href="/products?category=prebuilt">Prebuilt Computers</a></li>
+                <li> <a href="/products?category=gpu">GPUs</a></li>
+                <li><a href="/products?category=psu">PSUs</a>
                 </li>
             </ul>
         </nav>
