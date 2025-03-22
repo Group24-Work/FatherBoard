8346f4fc5e675199e88409b3cb218c94c554208e
LewisForster <lforst512@gmail.com>
2025-03-22 16:44:26 +0000

    attempt to shorten title of item on homepage

--- Fatherboard/resources/views/home.blade.php
+++ Fatherboard/resources/views/home.blade.php
@@ -26,7 +26,7 @@
         <h1 id=topTitle>Our Top Product!</h1>
             <img src="images/product_images/1.jpg" alt="RTX 4060">
             <div class="top-product-details">
-                <h3>{{ $topproduct->Title }}</h3>
+                <h3>{{ implode(' ', array_slice(explode(' ', $topproduct->Title), 0, 7)) }}</h3>
                 <a href="https://cs2team24.cs2410-web01pvm.aston.ac.uk/product/{{$topproduct-> id}}" class="shop-now">Shop Now</a>
             </div>
         </div>
