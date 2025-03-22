466a0c29aceaf49ab30b0fb7fb534fc4bd3053cf
LewisForster <lforst512@gmail.com>
2025-03-22 17:33:05 +0000

    updated routing for admin header

--- Fatherboard/resources/views/components/adminh.blade.php
+++ Fatherboard/resources/views/components/adminh.blade.php
@@ -9,7 +9,7 @@
         <nav class="main-nav">
             <ul class="main-nav-list">
                 <li>
-                    <a href="/settings">
+                    <a href="{{ route('settings') }}">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                             fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-user">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -23,12 +23,11 @@
                         <li><a id="logout_button">Logout</a></li>
                     </ul>
                 </li>
-                <li><a href="/basket"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-basket">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path
-                                d="M15.949 3.684l1.104 3.316h1.947a3 3 0 0 1 2.962 3.477l-1.252 7.131a4 4 0 0 1 -3.954 3.392h-9.512a3.994 3.994 0 0 1 -3.95 -3.371l-1.258 -7.173a3 3 0 0 1 2.964 -3.456h1.945l1.105 -3.316a1 1 0 0 1 1.898 .632l-.895 2.684h5.893l-.895 -2.684a1 1 0 1 1 1.898 -.632m-3.949 7.316a3 3 0 0 0 -2.995 2.824l-.005 .176a3 3 0 1 0 3 -3" />
-                        </svg></a></li>
+                <li><a href="{{ route('home') }}"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"
+                  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  
+                  class="icon icon-tabler icons-tabler-outline icon-tabler-logout"><path stroke="none" d="M0 0h24v24H0z" 
+                  fill="none"/><path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+                  <path d="M9 12h12l-3 -3" /><path d="M18 15l3 -3" /></svg></a></li>
             </ul>
         </nav>
     </div>
