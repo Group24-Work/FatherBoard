48c4ae42fe90609da3cd5c95f524820af33691cc
SaMEer27-10 <smrmahmood01@gmail.com>
2025-03-23 21:21:46 +0000

    updated product controller.php

--- Fatherboard/app/Http/Controllers/ProductController.php
+++ Fatherboard/app/Http/Controllers/ProductController.php
@@ -265,12 +265,13 @@ class ProductController extends Controller
         $curRating = DB::table("reviews")->where("product_id",$id)->select(DB::raw("avg(rating) as avg_rating"))->first();
         $image = "rtx2070.png";
         $amountStar = Utility::numberClosest($curRating->avg_rating, [1,2,3,4,5]);
-        if ($id <=25)
-        {
-            $image = $id . ".jpg";
-        }
         
-        $stock = $product->stock ?? 0; 
+        $image = ($id <= 25) ? 
+        
+        "$id.jpg" : "rtx2070.png";
+        
+        
+        $stock = optional ($product->stock()->first())->stock ?? 0; 
         $lowStockThreshold = 5; 
     
         $lowStockAlert = $stock < $lowStockThreshold ? "Warning: Low stock ({$stock} left)!" : null;
